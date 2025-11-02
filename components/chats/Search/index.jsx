299fdf9a92d7ad609f754b980968743a93474787
artiff3 <artem.ivan456@gmail.com>
2025-11-02 16:45:05 +0700

    Update Search

--- components/chats/Search/index.jsx
+++ components/chats/Search/index.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setSearchTerm } from "@/reducers/userSlice";
 import Button from "@/components/layout/Button";
-import styles from "@/assets/styles/components/Search/index.module.css"
+import styles from "@/assets/styles/components/Search/index.module.css";
+import {useRouter} from "next/router";
 
 const Search = () => {
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const [inputValue, setInputValue] = useState("");
 
-    return(
+    const handleSearch = () => {
+        dispatch(setSearchTerm(inputValue));
+        router.push({pathname: "/search", query: { q: inputValue },})
+    };
+
+    const handleKeyDown = e => {
+        if (e.key === "Enter") {
+            handleSearch();
+        }
+    };
+
+    return (
         <div className={styles.search__container}>
-            <input className={styles.search__input} placeholder="Searching the chats or users you need..." />
-            <Button>Search</Button>
+            <input
+                className={styles.search__input}
+                placeholder="Searching the chats or users you need..."
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+            <Button onClick={handleSearch}>Search</Button>
         </div>
-    )
-}
+    );
+};
 
 export default Search;
