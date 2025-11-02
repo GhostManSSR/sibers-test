5644a800da653cf8ad6bc4fa8330bd16c3e2b5bc
artiff3 <artem.ivan456@gmail.com>
2025-11-02 16:42:43 +0700

    Modified layout

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,20 +1,32 @@
-import Select from "@/components/layout/Select";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentUserName, setListNameUsers } from "@/reducers/userSlice";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import styles from "@/assets/styles/components/layout/index.module.css";
-import {useEffect, useState} from "react";
+import users from "@/data/users.json";
 
-const Layout = ({ ...props }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+const Layout = ({ children }) => {
+    const dispatch = useDispatch();
+    const { currentUser, currentUserName, listNameUsers } = useSelector(state => state.user);
+
+    useEffect(() => {
+        dispatch(setListNameUsers(users));
+    }, [dispatch]);
 
     return (
-        <>
-            <Header currentUser={currentUser} setCurrentUser={(value) => setCurrentUser(value)}/>
+        <div className={styles.layout__main}>
+            <Header
+                currentUser={currentUser}
+                listNameUsers={listNameUsers.map(user => ({ title: user.username }))}
+                currentUserName={currentUserName}
+                setCurrentUserName={(name) => dispatch(setCurrentUserName(name))}
+            />
             <div className={styles.layout__container}>
-                {props.children}
+                {children}
                 <Footer />
             </div>
-        </>
+        </div>
     );
 };
 
