b3cb4a2e3bcf0d2256ebcdc2a523226e029302df
artiff3 <artem.ivan456@gmail.com>
2025-11-03 11:28:13 +0700

    Update Input

--- components/layout/Input/index.jsx
+++ components/layout/Input/index.jsx
@@ -2,7 +2,7 @@ import styles from "@/assets/styles/components/layout/Input.module.css"
 
 const Input = ({...props}) => {
 
-    return <input className={props.className || styles.input_field} value={props.value} onChange={props.onChange} placeholder={props.placeholder} />
+    return <input onKeyPress={props.onKeyPress} disabled={props.disabled} type={props.htmlType || "text"} className={props.className || styles.input_field} value={props.value} onChange={props.onChange} placeholder={props.placeholder} />
 }
 
 export default Input;
